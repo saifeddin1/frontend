ad7310c2012c2d9f1e00f9b39f4ec2ea3728b331
saifeddin1 <saifeddinmatoui4@gmail.com>
2022-01-03 17:05:35 +0100

    Merged all changes

--- src/components/Timetables.js
+++ src/components/Timetables.js
@@ -14,45 +14,39 @@ import { HomeData } from './HomeData';
 const Timetables = () => {
     return (
         <div className="data timetables">
-            
+
             <div className="page-header">
                 <DashboardIcon id="Dicon" />
-                
-                    <h2 id="txt">Timetables </h2><br/>
-               
-                
-                
-                    <div>
-                   
-                    </div>
-                    
-                    </div>
-                    
-                    <h2>This is your timetables</h2>
-                                <Card sx={{ maxWidth: 150 }}  style={{ marginTop: '50px' }}>
-                    <CardMedia
-                       
-                        component="img"
-                        height="140"
-                        image= {Thumb}
-                    
-                        alt="Emploie"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                        
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                        
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small" onClick={()=> window.open("./course-thumb.png", "_blank")} >open</Button>
-                        <Button size="small">Delete</Button>
-                    </CardActions>
-                    </Card>
+                <h2 id="txt">Timetables </h2><br />
+
+
+            </div>
+
+            <h2>This is your timetables</h2>
+            <Card sx={{ maxWidth: 150 }} style={{ marginTop: '50px' }}>
+                <CardMedia
+
+                    component="img"
+                    height="140"
+                    image={Thumb}
+
+                    alt="Emploie"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small" onClick={() => window.open("./course-thumb.png", "_blank")} >open</Button>
+                    <Button size="small">Delete</Button>
+                </CardActions>
+            </Card>
         </div>
-        
+
     )
 }
 
